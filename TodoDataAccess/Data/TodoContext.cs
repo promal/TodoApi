04455726d2d81b1b0b36eb7eac28a0a0d0d0f770
Alejandro <alejandro@alejandros-mbp.home>
2020-05-18 16:13:50 -0700

    tutorial: asp.net core web api

---
+++ TodoDataAccess/Data/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoDomain.Models;
+
+namespace TodoDataAccess.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
